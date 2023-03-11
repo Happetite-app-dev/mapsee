ceec00966444150a99fe2e3093e5db85544e75c9
박정인 <>
2023-03-11 17:34:15 +0900

    no message

--- queries.js
+++ queries.js
@@ -1,13 +1,7 @@
 import { ref } from "@firebase/database";
 import { useState } from "react";
 import { useQuery, useQueries } from "react-query";
-import {
-  fetchFolder,
-  fetchUser,
-  fetchRecord,
-  fetchAllNotice,
-} from "./actions";
-
+import { fetchFolder, fetchUser, fetchRecord, fetchAllNotice } from "./actions";
 
 export const useUserQuery = (UID) =>
   useQuery(["users", UID], () => fetchUser(UID));
@@ -16,30 +10,38 @@ export const useFolderQuery = (folderID) =>
   useQuery(["folders", folderID], () => fetchFolder(folderID));
 
 export const useFolderQueries = (folderIDList) =>
-  useQueries(folderIDList.map((folderID) => {
-    return {
-      queryKey: ["folders", folderID],
-      queryFn: () => fetchFolder(folderID)
-    }
-  }))
+  useQueries(
+    folderIDList.map((folderID) => {
+      return {
+        queryKey: ["folders", folderID],
+        queryFn: () => fetchFolder(folderID),
+      };
+    })
+  );
 
 export const useRecordQuery = (recordID) =>
   useQuery(["records", recordID], () => fetchRecord(recordID));
 
 export const useRecordQueries = (folderIDList) => {
-  const folderQueries = useFolderQueries(folderIDList)
+  const folderQueries = useFolderQueries(folderIDList);
   const recordIDList = folderIDList.reduce((acc, curr, idx) => {
-    const folderObj = folderQueries[idx].data
-    const newRecordID = folderObj?.placeRecords ? Object.values(folderObj.placeRecords).map((item) => Object.keys(item)[0]) : [];
-    return [...acc, ...newRecordID]
-  }, new Array)
-  return useQueries(recordIDList.map((recordID) => {
-    return {
-      queryKey: ["records", recordID],
-      queryFn: () => fetchRecord(recordID)
-    }
-  }))
-}
+    const folderObj = folderQueries[idx].data;
+    const newRecordID = folderObj?.placeRecords
+      ? Object.values(folderObj.placeRecords).map(
+          (item) => Object.keys(item)[0]
+        )
+      : [];
+    return [...acc, ...newRecordID];
+  }, new Array());
+
+  return useQueries(
+    recordIDList.map((recordID) => {
+      return {
+        queryKey: ["records", recordID],
+        queryFn: () => fetchRecord(recordID),
+      };
+    })
+  );
+};
 export const useAllNoticeQuery = (UID) =>
   useQuery(["all-notices"], () => fetchAllNotice(UID));
-
